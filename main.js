aa1b0082e621b92883b43e2e9eb2f7f93ed537d4
fanylouise <gandra.fany@gmail.com>
2023-09-17 16:34:19 -0300

    mockup and third screen ok

--- main.js
+++ main.js
@@ -1,91 +1,79 @@
+/* Selecting elements from the HTML using the
+`document.querySelector()` and `document.getElementById()` methods. */
+const searchInput = document.querySelector('.search')
+const seeWeather = document.querySelector('.getData')
+const key = 'RN2K9W7RXKDS9HFUBNQWDL6UL'
+const temperature = document.querySelector('.temperature')
+const condition = document.querySelector('.condition')
+const city = document.querySelector('.city')
+const day = document.querySelector('.day')
+const thermometricScale = document.querySelector('.scale')
+const tempIcon = document.querySelector('.tempIcon')
+const windSpeed = document.querySelector('.windStat')
+const humidity = document.querySelector('.humidityStat')
+const visibility = document.querySelector('.visibilityStat')
+const airPressure = document.querySelector('.airPressureStat')
+const weatherToday = document.getElementById('#weatherToday')
+const hightlights = document.getElementById('#hightlights')
+const weatherWeekCard = document.querySelector('.weatherWeekCard')
+const week = document.querySelector('.week')
+const scroll = document.querySelector('.scroll')
 
-let searchInput = document.querySelector('.search');
-let seeWeather  = document.querySelector('.getData');
-let key = 'RN2K9W7RXKDS9HFUBNQWDL6UL';
-let temperature = document.querySelector('.temperature');
-let condition = document.querySelector('.condition');
-let city = document.querySelector('.city');
-let day = document.querySelector('.day');
-let thermometricScale = document.querySelector('.scale');
-let tempIcon = document.querySelector('.tempIcon');
-let windSpped = document.querySelector('.windStat')
-let humidity = document.querySelector('.humidityStat');
-let visibility = document.querySelector('.visibilityStat');
-let airPressure = document.querySelector('.airPressureStat');
-let header = document.getElementById('#header');
-let footer = document.getElementById('#footer');
-let weatherToday = document.getElementById('#weatherToday');
-let hightlights = document.getElementById('#hightlights');
-let main = document.getElementById('#main')
-let getDate = new Date();
-let week = document.querySelector('.week');
-let changeIcon = (weather)=>{
-    switch(weather.currentConditions.icon){
-    case "clear-day":
-      tempIcon.src = './assets/amcharts_weather_icons_1.0.0/static/day.svg';
-      break;
-    case "snow":
-      tempIcon.src ='./assets/amcharts_weather_icons_1.0.0/animated/snow-6.svg';
-      break;
-    case "rain":
-       tempIcon.src ='./assets/amcharts_weather_icons_1.0.0/animated/rainy-7.svg';
-       break;
-    case "fog":
-      tempIcon.src ='./assets/amcharts_weather_icons_1.0.0/animated/cloudy.svg';
-      break;
-    case "wind":
-      tempIcon.src ='./assets/amcharts_weather_icons_1.0.0/animated/day.svg';
-      break;
-    case "cloudy":
-      tempIcon.src ='./assets/amcharts_weather_icons_1.0.0/animated/cloudy.svg';
-      break;
-    case 'partly-cloudy-day':
-      tempIcon.src ='./assets/amcharts_weather_icons_1.0.0/animated/cloudy-day-1.svg';
-      break;
-    case 'partly-cloudy-night':
-      tempIcon.src ='./assets/amcharts_weather_icons_1.0.0/animated/cloudy-night-3.svg';
-      break;
-    case 'clear-night':
-      tempIcon.src ='./assets/amcharts_weather_icons_1.0.0/animated/night.svg';
-      break;
-    case 'overcasting':
-      tempIcon.src ='./assets/amcharts_weather_icons_1.0.0/animated/cloudy.svg';
-      break;
-    default:
-      tempIcon.src ='./assets/amcharts_weather_icons_1.0.0/static/weather.svg';
-      break;
-}
-}
 
-async function setData(){
 
-  if(searchInput.value != '' ){
-     let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${searchInput.value}?unitGroup=us&key=${key}`;
+ /* The `setData` function is an asynchronous function that is triggered when the user
+clicks on the "See Weather" button. It retrieves weather data from an API based on
+the value entered in the search input field. The function makes a fetch request to
+the API endpoint, waits for the response, and then parses the response as JSON. It
+then updates various elements on the page with the retrieved weather data, such as
+temperature, city, condition, wind speed, humidity, visibility, air pressure, and the
 
-  const response = await fetch(url);
-  const weather = await response.json();
 
+weather forecast for the week. */
+async function setData() {
   
-  console.log(weather)
-  
-  temperature.innerHTML = Math.floor((weather.currentConditions.temp - 32) / 1.8);
-  city.innerHTML = weather.address;
-  condition.innerHTML = weather.currentConditions.conditions;
-  thermometricScale.innerHTML = '°C';
-  tempIcon.alt = weather.description;
-  windSpped.innerHTML= weather.currentConditions.windspeed + ' mph';
-  humidity.innerHTML = weather.currentConditions.humidity + ' %';
-  visibility.innerHTML = weather.currentConditions.visibility + ' miles';
-  airPressure.innerHTML = weather.currentConditions.pressure + ' mb';
+  if(searchInput.value != '') {
 
-  changeIcon(weather);
-  }else{
+    let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${searchInput.value}?unitGroup=us&key=${key}`
+
+    const response = await fetch(url)
+    const weather = await response.json()
+
+    console.log(weather)
+
+    temperature.innerHTML = Math.floor(
+      (weather.currentConditions.temp - 32) / 1.8
+    )
+    city.innerHTML = weather.address.toUpperCase()
+    condition.innerHTML = weather.currentConditions.conditions
+    thermometricScale.innerHTML = '°C'
+    tempIcon.alt = weather.description
+    windSpeed.innerHTML = weather.currentConditions.windspeed + ' mph'
+    humidity.innerHTML = weather.currentConditions.humidity + ' %'
+    visibility.innerHTML = weather.currentConditions.visibility + ' miles'
+    airPressure.innerHTML = weather.currentConditions.pressure + ' mb'
+    tempIcon.src = `./assets/amcharts_weather_icons_1.0.0/animated/${weather.currentConditions.icon}.svg`
+
+    week.innerHTML = ''
+
+    for (dia of weather.days) {
+      week.innerHTML += `<div class='weatherWeekCard'>
+
+
+       <img src='${`./assets/amcharts_weather_icons_1.0.0/animated/${dia.icon}.svg`}' alt='' class='tempIcon'>;
+       
+      <section class='minmax'>
+        <span class="max">${Math.floor((dia.tempmax - 32) / 1.8)} °C </span>
+        <span class="min">${Math.floor((dia.tempmin - 32) / 1.8)} °C </span>
+      </section>
+     
+      <h3 class='title' >${dia.datetime.slice(5.6)}</h3>
+      </div>
+      `
+    }
+  } else {
     alert('Please, enter the city.')
   }
-
 }
-
-
-
 
 seeWeather.addEventListener('click', setData)
